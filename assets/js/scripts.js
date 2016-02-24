28a11052d213e498419657a16fd7a49ccddae6aa
Jeremy Smith <smithjeremym@gmail.com>
2016-02-23 22:05:08 -0500

    get jquery working on tabs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,14 +7,25 @@ $(document).ready( function () {
     } else { // if the tab clicked is NOT the active tab
       $(this).addClass("activeTab"); // add class active to the tab that was clicked
       $(this).siblings().removeClass("activeTab"); //remove class active from other tabs
-      for (var i=0; i < $(".tabContainer").length; i++){
-        var iElement = $(".tabContainer")[i];
-        if ($(iElement).attr("class").includes("hidden")) {
-          $(iElement).removeClass("hidden");
-        } else {
-          $(iElement).addClass("hidden");
-        }
+
+      var contributions = $(".contributionsTab")[1];//set variables for all three sections to show/hide
+      var publicActivity = $(".publicActivityTab")[1];
+      var repositories = $(".repositoriesTab")[1];
+
+      if ($(this).attr("class").includes("contributionsTab")) { // if the contributions Tab is clicked
+        $(contributions).removeClass("hidden");//show the appropriate div
+        $(publicActivity).addClass("hidden");//hide the
+        $(repositories).addClass("hidden");//other two divs
+      } else if ($(this).attr("class").includes("repositoriesTab")) { // if the repositories tab is clicked
+        $(repositories).removeClass("hidden");// et
+        $(contributions).addClass("hidden");// cetera
+        $(publicActivity).addClass("hidden");
+      } else { // if the publicActivityTab is clicked
+        $(publicActivity).removeClass("hidden");//et
+        $(contributions).addClass("hidden");//cetera
+        $(repositories).addClass("hidden");
       }
+
     }
 
   });
