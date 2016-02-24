b585bf3d39d12a9b6a98ab2ab0ab44e0c92ad078
Jeremy Smith <smithjeremym@gmail.com>
2016-02-24 18:30:11 -0500

    start building out json-populated repo lists

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+//see if I can't get the 'user' name plugged in to wherever it's needed
+
 $(document).ready( function () {
   "use strict";
 
@@ -9,7 +11,7 @@ $(document).ready( function () {
       $(this).siblings().removeClass("activeTab"); //remove class active from other tabs
 
       $(this).children().filter("div").addClass("activeBorder"); //hide the bottom border with an absolutely positioned div
-     $(this).siblings().children().filter("div").removeClass("activeBorder"); // show the bottom border again
+      $(this).siblings().children().filter("div").removeClass("activeBorder"); // show the bottom border again for non-clicked items
 
 
       var contributions = $(".contributionsTab")[1];//set variables for all three sections to show/hide
@@ -35,7 +37,7 @@ $(document).ready( function () {
   });
 
   // some of these are not pulling in correctly. We will continue to troubleshoot them
-  $.getJSON( "https://api.github.com/users/mike-crane", function( json ) {
+  $.getJSON( "https://api.github.com/users/octocat", function( json ) {
     var time = new Date(json.created_at).toLocaleDateString();
 
      $(".octocatImage").append('<img src="'+json.avatar_url+'" alt="avatar"></img>');
@@ -51,9 +53,38 @@ $(document).ready( function () {
      $(".following").html(json.following);
   });
 
-//let's replace the json.location api information with a lodash template-sourced thing
+  // if we use lodash for local json or remote json, we will need to modify the statment below instead of hard coding the thing like we did for 'Raleigh, NC'
   var greeting = _.template('<%- p.location %>', { variable: 'p' });
   var renderedHtml = greeting({ location: 'Raleigh, NC' });
   $(".city").html(renderedHtml);
+
+  $.getJSON( "https://api.github.com/users/octocat/repos", function( json ) { //json for the contents under the 'Contributions' tab
+
+    for (var i=0; i<5; i++){
+      // $(".repoTitle").html(json[i].full_name);//this works!
+      //I will need to append a new list item every time the thing iterates
+      $(".repoTitle").append('<li class="repoTitle"><span class="octicon octicon-repo"></span>'+json[i].full_name+'</li>');//this does some stuff! But it sucks!
+    }
+
+  });
+
+  // $.getJSON( "ajax/test.json", function( data ) {
+  // var items = [];
+  // $.each( data, function( key, val ) {
+  //   items.push( "<li id='" + key + "'>" + val + "</li>" );
+  // });
+  //
+  // $( "<ul/>", {
+  //   "class": "my-new-list",
+  //   html: items.join( "" )
+  //   }).appendTo( "body" );
+  // });
+
+//arrayName[objectNumber].objectAttribute
+
+// below are things to research
+// //let's replace the json.location api information with a lodash template-sourced thing
+//
+//   $.ajax
 
 });
